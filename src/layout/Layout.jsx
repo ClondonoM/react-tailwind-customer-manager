edb98c65688823071434003eccef720277b9be22
Carlos Londono <c.londono.m@gmail.com>
2022-07-06 10:20:47 -0500

    styling Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,38 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
+
 const Layout = () => {
+  const location = useLocation();
+  const activeUrl = location.pathname;
+
   return (
-    <div>
-      <h1>From Layout</h1>
-      <Outlet />
+    <div className='md:flex md:min-h-screen'>
+      <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
+        <h2 className='text-4xl font-black text-center text-white'>
+          CRM - Customers
+        </h2>
+        <nav className='mt-10 '>
+          <Link
+            className={`${
+              activeUrl === '/customers' ? 'text-blue-300' : 'text-white'
+            } text-2xl block mt-2 hover:text-blue-300`}
+            to='/customers'
+          >
+            Customers
+          </Link>
+
+          <Link
+            className={`${
+              activeUrl === '/customers/new' ? 'text-blue-300' : 'text-white'
+            } text-2xl block mt-2 hover:text-blue-300`}
+            to='/customers/new'
+          >
+            New Customer
+          </Link>
+        </nav>
+      </div>
+      <div className='md:w-3/4'>
+        <Outlet />
+      </div>
     </div>
   );
 };
